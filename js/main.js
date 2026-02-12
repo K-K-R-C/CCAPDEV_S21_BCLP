592b66136d70699d03d1b12c27c8b86c2d170ab3
OphieLuc <sofia_lucena@dlsu.edu.ph>
2026-02-12 23:53:33 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,47 @@
 //To be finished for MCO#2
+
+// localStorage.removeItem("isLoggedIn");
+// ^^ uncomment this when testing out guest mode first
+
+let isLoggedIn = false;
+
+let loggedIn = document.getElementById("loggedInContainer");
+let loggedOut = document.getElementById("loggedOutContainer");
+
+if (localStorage.getItem("isLoggedIn") === "true") {
+    isLoggedIn = true;
+}
+
+if (loggedIn && loggedOut) {
+    if (isLoggedIn) {
+        loggedOut.style.display = "none";
+        loggedIn.style.display = "block";
+    } else {
+        loggedIn.style.display = "none";
+        loggedOut.style.display = "block";
+    }
+}
+
+let registerButton = document.getElementById("registerButton");
+if (registerButton) {
+    registerButton.addEventListener("click", function() {
+        localStorage.setItem("isLoggedIn", "true");
+        window.location.href = "index.html";
+    });
+}
+
+let loginButton = document.getElementById("loginButton");
+if (loginButton) {
+    loginButton.addEventListener("click", function() {
+        localStorage.setItem("isLoggedIn", "true");
+        window.location.href = "index.html";
+    });
+}
+
+let logoutButton = document.getElementById("logoutButton");
+if (logoutButton) {
+    logoutButton.addEventListener("click", function() {
+        localStorage.removeItem("isLoggedIn");
+        window.location.href = "index.html";
+    });
+}
