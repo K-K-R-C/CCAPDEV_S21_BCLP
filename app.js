5e1dc8bb04adc9aec6ea20e789d21ae7fde0337d
Trisha <trisha_bendol@dlsu.edu.ph>
2026-03-08 00:37:25 +0800

    added register.hbs and edited app.js

--- app.js
+++ app.js
@@ -16,8 +16,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Server is running");
+    res.render("index");
 });
+
+app.get("/profile", (req, res) => {
+    res.render("profile");
+});
+
+app.get("/create-post", (req, res) => {
+    res.render("create-post");
+});
+
+app.get("/post", (req, res) => {
+    res.render("post");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
