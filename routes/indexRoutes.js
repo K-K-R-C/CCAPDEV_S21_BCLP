b687b6e802015e1adfdefd1056ad92ed36ef65f3
Trisha <trisha_bendol@dlsu.edu.ph>
2026-04-05 22:58:41 +0800

    added new routes

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -8,12 +8,24 @@ const postController = require("../controllers/postController");
 const userController = require("../controllers/userController");
 const commentController = require("../controllers/commentController");
 
+// Create and Get Posts
 router.get("/", postController.getAllPosts);
 router.get("/post/:id", postController.getPost);
-router.get("/user/:username", postController.getUserProfile);
 router.get("/create-post", postController.showCreatePost);
 router.post("/posts", upload.array("photo", 5), postController.createPost);
+
+// User Profile
+router.get("/user/:username", postController.getUserProfile);
+
+// Edit and Delete Posts
+router.get("/post/:id/edit", postController.showEditPost);
+router.post("/post/:id/edit", upload.array("photo", 5), postController.editPost);
+router.post("/post/:id/delete", postController.deletePost);
+
+// Comments
 router.post("/post/:id/comment", commentController.createComment);
+
+// Register and Login
 router.get("/register", userController.showRegister);
 router.post("/register", userController.register);
 router.get("/login", userController.showLogin);
